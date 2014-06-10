af2b9c508bc1881af26daf01c37cc11b16d482fc
Todd Michael Bushnell <toddmichael@gmail.com>
2014-06-10 11:26:41 -0700

    bump version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'toddmichael@gmail.com'
 license 'All rights reserved'
 description 'A simple Tomcat cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 supports 'centos'
 
