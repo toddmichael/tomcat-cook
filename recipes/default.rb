4674f029dbf59455a5b3189b29f3a1f4c32346e9
Todd Michael Bushnell <toddmichael@gmail.com>
2014-09-16 07:00:34 -0700

    bugfix: default recipe was not calling my _java recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,7 @@ unless platform_family?('rhel')
   Chef::Application.fatal!("Cookbook incompatible with #{platform_family?}")
 end
 
-# Install Java using Opscode Java cookbook
-include_recipe 'java'
+include_recipe 'tomcat::_java'
 
 # Only support Ark driven binary tarball install presently
 include_recipe 'tomcat::_ark'
