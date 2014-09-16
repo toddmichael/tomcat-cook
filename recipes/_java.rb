3eb9b39cd1c9bc8c20f6b08351ed97e19d5ecc7f
Todd Michael Bushnell <toddmichael@gmail.com>
2014-09-16 07:00:15 -0700

    require java >= 7 if using tomcat 8

--- recipes/_java.rb
+++ recipes/_java.rb
@@ -2,4 +2,13 @@
 # Recipe:: _java
 #
 
+tomcat_version = node[:tomcat][:version].to_i
+java_version = node[:java][:jdk_version].to_i
+
+# tomcat 8 requires java >= 7
+# http://tomcat.apache.org/migration-8.html#Java_7_required
+if tomcat_version == 8 and java_version < 7
+  Chef::Application.fatal!("Tomcat 8 requires Java >= 7. You have Java #{java_version}")
+end
+
 include_recipe 'java'
