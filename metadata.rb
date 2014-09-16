f5557e73d272326f4943bb7dd188a554f2bb83e6
Todd Michael Bushnell <toddmichael@gmail.com>
2014-09-16 08:28:02 -0700

    switch license to Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'tomcat'
 maintainer 'Todd Michael Bushnell'
 maintainer_email 'toddmichael@gmail.com'
-license 'All rights reserved'
+license 'Apache 2.0'
 description 'A simple Tomcat cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.2'
